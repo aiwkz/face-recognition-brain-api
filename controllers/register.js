ee5982492abdc8e52e059b9cba868f235519e0d5
aiwkz <federicoj.pp@gmail.com>
2023-05-29 17:30:16 +0200

    🎨 adds guard to avoid empty requests

--- controllers/register.js
+++ controllers/register.js
@@ -1,5 +1,8 @@
 const handleRegister = (req, res, bcrypt, db) => {
     const { name, email, password } = req.body;
+    if(!name || !email || !password) {
+        return res.status(400).json('Complete all the fields before submitting');
+    }
     const hash =bcrypt.hashSync(password);
     db.transaction(trx => {
         trx.insert({
