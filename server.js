d6e29f39f35ce0914c70dd5ab3c33fbb483c470f
aiwkz <federicoj.pp@gmail.com>
2023-06-02 15:38:12 +0200

    🚧 more fixes for cors

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ const db = knex({
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
