4f0f046f515fd331c3d2e6ca8def0fa0d94b2f9f
aiwkz <federicoj.pp@gmail.com>
2023-06-05 16:09:59 +0200

    🚧 more cors errors

--- server.js
+++ server.js
@@ -27,12 +27,7 @@ const db = knex({
 const app = express();
 
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-  res.set('Access-Control-Allow-Origin', '*');
-  res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.set('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('success')
