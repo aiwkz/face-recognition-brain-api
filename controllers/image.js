5ff37554ed3df25fdb019a58bf2901ffce1b1d0b
aiwkz <federicoj.pp@gmail.com>
2023-06-06 15:47:26 +0200

    🐛 replacing fetch for https request

--- controllers/image.js
+++ controllers/image.js
@@ -1,41 +1,54 @@
-const clarifaiApiCall = (req, res) => { 
-    const PAT='10881e14e6ec49e185af430e206583ca'
-    const USER_ID='aiwkz'
-    const APP_ID='face-recognition-brain'
-    const MODEL_ID='face-detection'
-    const IMAGE_URL = req.body.input;
+const https = require('https');
 
-    const raw = JSON.stringify({
-        "user_app_id": {
-          "user_id": USER_ID,
-          "app_id": APP_ID
+const clarifaiApiCall = (req, res) => {
+  const PAT = '10881e14e6ec49e185af430e206583ca';
+  const USER_ID = 'aiwkz';
+  const APP_ID = 'face-recognition-brain';
+  const MODEL_ID = 'face-detection';
+  const IMAGE_URL = req.body.input;
+   const data = JSON.stringify({
+    user_app_id: {
+      user_id: USER_ID,
+      app_id: APP_ID,
+    },
+    inputs: [
+      {
+        data: {
+          image: {
+            url: IMAGE_URL,
+          },
         },
-        "inputs": [
-          {
-            "data": {
-              "image": {
-                "url": IMAGE_URL
-              }
-            }
-          }
-        ]
+      },
+    ],
+  });
+   const options = {
+    hostname: 'api.clarifai.com',
+    path: `/v2/models/${MODEL_ID}/outputs`,
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      Authorization: `Key ${PAT}`,
+      'Content-Type': 'application/json',
+      'Content-Length': data.length,
+    },
+  };
+   const request = https.request(options, (response) => {
+    let responseData = '';
+     response.on('data', (chunk) => {
+      responseData += chunk;
     });
-
-    const requestOptions = {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Authorization': 'Key ' + PAT,
-        },
-        body: raw,
-    };
-
-    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", requestOptions)
-      .then(response => response.json())
-      .then(result => res.json(result))
-      .catch(error => console.log('error', error));
-}
-
-module.exports = {
-    clarifaiApiCall
+     response.on('end', () => {
+      const result = JSON.parse(responseData);
+      res.json(result);
+    });
+  });
+   request.on('error', (error) => {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while processing your request.' });
+  });
+   request.write(data);
+  request.end();
+};
+ module.exports = {
+  clarifaiApiCall,
 };
